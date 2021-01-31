bc3e05243eee3a855f1346894ac45e761dc83f91
Renan Reis <renan.reis@esig.com.br>
2021-01-31 00:49:41 -0300

    Refatorando ListComponent

--- src/pages/Home/HomeComponent.vue
+++ src/pages/Home/HomeComponent.vue
@@ -9,19 +9,19 @@
       <div>
         <div class="row">
           <div class="col-12 col-md-3">
-            <CardsComponent :type="'Psicólogos'" :percentage="'7%'" :icon="'fa-users'" :qtd="'250'" :bg="'#4CAF4F'" />
+            <CardsComponent :type="'Clientes'" :percentage="'7%'" :icon="'fa-users'" :qtd="'250'" :bg="'#4CAF4F'" />
           </div>
 
           <div class="col-12 col-md-3">
-            <CardsComponent :type="'Pacientes'" :percentage="'7%'" :icon="'fa-users'" :qtd="'198'" :bg="'#FB9801'" />
+            <CardsComponent :type="'Matrículas'" :percentage="'7%'" :icon="'fa-users'" :qtd="'198'" :bg="'#FB9801'" />
           </div>
 
           <div class="col-12 col-md-3">
-            <CardsComponent :type="'Consultas'" :percentage="'7%'" :icon="'fa-users'" :qtd="'788'" :bg="'#30CAE3'" />
+            <CardsComponent :type="'Mensalidades'" :percentage="'7%'" :icon="'fa-users'" :qtd="'788'" :bg="'#30CAE3'" />
           </div>
 
           <div class="col-12 col-md-3">
-            <CardsComponent :type="'Cancelamentos'" :percentage="'7%'" :icon="'fa-users'" :qtd="'523'" :bg="'#7872CF'" />
+            <CardsComponent :type="'Relatórios'" :percentage="'7%'" :icon="'fa-users'" :qtd="'523'" :bg="'#7872CF'" />
           </div>
         </div>
       </div>
